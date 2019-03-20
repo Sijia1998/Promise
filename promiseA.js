53a8c82179072cad6329b461e48a94f2adf283a0
Sijia1998 <zhu19980304@163.com>
2019-03-20 11:30:58 +0800

    push

--- promiseA.js
+++ promiseA.js
@@ -5,25 +5,34 @@ const REJECTED = "rejected";
 function MyPromise(fn) {
   const that = this; // 代码可能会异步执行，用于获取正确的 this 对象
   this.state = PENDING; // 一开始 Promise 的状态应该是 pending
-  that.value = null; // value变量用于保存传入 resolve 或者 reject 中的值
+  that.value = null; // value 变量用于保存 resolve 或者 reject 中传入的值
   // 用于保存then中的回调
   that.resolevdCallbacks = [];
   that.rejectedCallbacks = [];
   // resolve函数
   function resolve(value) {
-    if (that.state == PENDING) {
-      that.state = RESOLVED;
-      that.value = value;
-      that.resolevdCallbacks.map(cb => cb(that.value));
+    // 判断传入的值是否为 Promise 类型
+    if (value instanceof MyPromise) {
+      return value.then(resolve, reject)
     }
+    setTimeout(() => {
+      if (that.state == PENDING) {
+        that.state = RESOLVED;
+        that.value = value;
+        that.resolevdCallbacks.map(cb => cb(that.value));
+      }
+    }, 0)
   }
+
   // reject函数
   function reject(value) {
-    if (that.state == PENDING) {
-      that.state = RESOLVED;
-      that.value = value;
-      that.rejectedCallbacks.map(cb => cb(that.value));
-    }
+    setTimeout(() => {
+      if (that.state === PENDING) {
+        that.state = REJECTED
+        that.value = value
+        that.rejectedCallbacks.map(cb => cb(that.value))
+      }
+    }, 0)
   }
 
   try {
@@ -44,8 +53,25 @@ MyPromise.prototype.then = function (onFulfilled, onRejected) {
       throw r;
     };
   if (that.state === PENDING) {
-    that.resolevdCallbacks.push(onFulfilled);
-    that.rejectedCallbacks.push(onRejected);
+    return (promise2 = new MyPromise((resolve, reject) => {
+      that.resolvedCallbacks.push(() => {
+        try {
+          const x = onFulfilled(that.value)
+          resolutionProcedure(promise2, x, resolve, reject)
+        } catch (r) {
+          reject(r)
+        }
+      })
+
+      that.rejectedCallbacks.push(() => {
+        try {
+          const x = onRejected(that.value)
+          resolutionProcedure(promise2, x, resolve, reject)
+        } catch (r) {
+          reject(r)
+        }
+      })
+    }))
   }
   if (that.state === RESOLVED) {
     onFulfilled(that.value);
@@ -61,4 +87,4 @@ new MyPromise((resolve, reject) => {
   }, 0);
 }).then(value => {
   console.log(value);
-})
+});
